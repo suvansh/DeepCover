8e2390bc5c831bdab373f4f3b44ced0c19d271fa
suvansh <suvansh.sanjeev@gmail.com>
2021-03-02 06:11:20 -0800

    adding datetime to str util function

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,9 @@
 from os.path import dirname
+from datetime import datetime
 
 def get_repo_dir():
     """ Can be called from anywhere to get project root directory """
     return dirname(dirname(__file__))
+
+def datetime_str(dt=None):
+    return datetime.strftime(dt or datetime.now(), '%m%d%Y_%H%M%S')
