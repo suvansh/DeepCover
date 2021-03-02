8e9dfa97ddfd58643248ec18b34a21fa5b3ac9ee
suvansh <suvansh.sanjeev@gmail.com>
2021-03-02 02:02:11 -0800

    Adding initial notebook with data processing and (untested) inner model

--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,6 @@
 try:
-    from config_personal import *
+    from .config_personal import *
+    print(f'Using personal config for user: {user}')
 except ImportError:
-    from config_template import *
+    from .config_template import *
+    print('Using template config')
