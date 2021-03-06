842e229029cced8145b80d2ac397715d9e998755
Suvansh Sanjeev <suvansh@cmu.edu>
2021-03-06 02:18:39 -0500

    adding user to config template

--- src/config/config_template.py
+++ src/config/config_template.py
@@ -2,4 +2,5 @@ from utils import get_repo_dir
 from os.path import join
 
 # NOTE copy this file, update the below variables with the correct paths
+user = 'my_name'
 data_dir = join(get_repo_dir(), 'data')  # path to your data directory
