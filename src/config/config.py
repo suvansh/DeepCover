e841a64bd9110fcab7c53c3f0607af04381c0d90
Udit Ranasaria <uditrana@outlook.com>
2021-05-29 14:12:27 -0700

    small qol improvements

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,6 @@
 try:
     from .config_personal import *
-    print(f'Using personal config for user: {user}')
+    print(f'Using personal config for user: {user} and data_dir: {data_dir}')
 except ImportError:
     from .config_template import *
     print('Using template config')
